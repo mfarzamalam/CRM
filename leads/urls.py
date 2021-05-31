fab308b22a97d53305fef8c6f12ca83d1b39e478
mfarzamalam <mfarzamalam@gmail.com>
2021-05-31 18:31:09 +0500

    crud done by generic view

--- leads/urls.py
+++ leads/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import lead, lead_detail, create_lead
+from .views import lead, lead_detail, create_lead, update_lead, delete_lead
 
 app_name = 'leads'
 
@@ -7,4 +7,9 @@ urlpatterns = [
     path('', lead),
     path('detail/<int:pk>/', lead_detail),
     path('create/', create_lead),
+    path('update/<int:pk>', update_lead, name='update'),
+    path('delete/<int:pk>', delete_lead, name='delete'),
+
+
+    
 ]
