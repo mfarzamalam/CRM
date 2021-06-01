633ee8743f5b0f9757fcc7d8537f668988699cf5
mfarzamalam <mfarzamalam@gmail.com>
2021-06-01 20:12:03 +0500

    added base file with template inheritance

--- CRM/urls.py
+++ CRM/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from leads.views import LandingPageView
+from django.contrib.auth.views import LoginView,LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LandingPageView.as_view(), name='home'),
-    path('leads/', include('leads.urls', namespace='leads'))
+    path('leads/', include('leads.urls', namespace='leads')),
+    # path('signup/', .as_view(), name='signup'),
+    path('login/', LoginView.as_view(template_name='leads/registration/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
