54eb710a9a6bdda096004449dc8fa2831b885eff
mfarzamalam <mfarzamalam@gmail.com>
2021-06-02 01:50:02 +0500

    agent crud using modelform and class based generic view

--- agents/urls.py
+++ agents/urls.py
@@ -4,5 +4,9 @@ from .views import AgentListView, AgentCreateView, AgentDetailView, AgentUpdateV
 app_name = "agents"
 
 urlpatterns = [
-    path('list/', AgentListView, name='list'),
+    path('list/', AgentListView.as_view(), name='list'),
+    path('create/', AgentCreateView.as_view(), name='create'),
+    path('detail/<int:pk>', AgentDetailView.as_view(), name='detail'),
+    path('update/<int:pk>', AgentUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', AgentDeleteView.as_view(), name='delete'),
 ]
--- agents/views.py
+++ agents/views.py
@@ -1,20 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 from django.views.generic import DetailView, ListView, UpdateView, DeleteView, CreateView
 from leads.models import Agent
+from .forms import AgentModelForm
 
 # Create your views here.
+class AgentListView(ListView):
+    queryset            = Agent.objects.all()
+    context_object_name = "agents"
+    template_name       = 'agents/agent_list.html'
 
-class AgentCreateView():
-    pass
 
-class AgentListView():
-    pass
+class AgentCreateView(CreateView):
+    form_class    = AgentModelForm
+    template_name = 'agents/agent_create.html'
 
-class AgentUpdateView():
-    pass
+    def get_success_url(self):
+        return reverse('agents:list')
 
-class AgentDetailView():
-    pass
 
-class AgentDeleteView():
-    pass
+class AgentDetailView(DetailView):
+    queryset            = Agent.objects.all()
+    context_object_name = "agent"
+    template_name       = 'agents/agent_detail.html'
+
+
+class AgentUpdateView(UpdateView):
+    form_class    = AgentModelForm
+    queryset      = Agent.objects.all()
+    template_name = 'agents/agent_update.html'
+
+    def get_success_url(self):
+        return reverse('agents:list')
+
+
+class AgentDeleteView(DeleteView):
+    queryset      = Agent.objects.all()
+    template_name = 'agents/agent_delete.html'
+
+    def get_success_url(self):
+        return reverse('agents:list')
